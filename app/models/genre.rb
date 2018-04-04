e1ca04c1164e572b573bfe10f581e147a591636b
Ariana Berger <ariana.berger@gmail.com>
2018-04-04 03:10:15 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -16,11 +16,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    # self.artists.each {|a| a.name}
-    self.artists.map do |a|
-      a.name
-      binding.pry
-
+    self.artists.map {|a| a.name}
     end
   end
 end
